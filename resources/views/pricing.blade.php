75c2adb3ae4e1b83dff27464f204e686529ee189
contactithrytec <contact@ithrytec.com>
2023-05-03 16:20:52 +0100

    some changes in pricing

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -20,100 +20,93 @@
                     <div class="text-center mb-5 radio-box-wrap billingCycle">
                         <div class="single-radio-box">
                             <input name="billingPlan" id="monthly-plan" value="monthly" class="radio" type="radio" checked>
-                            <label for="monthly-plan"><span class="custom-check"></span> Monthly</label>
-                        </div>
-                        <div class="single-radio-box">
-                            <input name="billingPlan" id="yearly-plan" value="yearly" class="radio" type="radio">
-                            <label for="yearly-plan"><span class="custom-check"></span> Yearly</label>
+                            <label for="monthly-plan"><span class="custom-check"></span> Nos Tarifs </label>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row align-items-center justify-content-between">
-                <div class="col-lg-4 col-md-4 col-12">
-                    <div class="card text-center single-pricing-pack">
-                        <div class="pt-5">
-                            <h5 class="mb-0">Cloud Starter</h5>
-                            <p class="mb-0">Medium traffic personal sites</p>
-                        </div>
-                        <div class="card-header pb-4 border-0 pricing-header">
-                            <div class="price text-center mb-0 monthly-price">$19<span>/month</span></div>
-                            <div class="price text-center mb-0 yearly-price">$69<span>/yearly</span></div>
-                            <div class="price text-center mb-0 biannual-price">$130<span>/biannual</span></div>
-                            <div class="price text-center mb-0 triennial-price">$199<span>/triennial</span></div>
-                        </div>
-                        <div class="card-body">
-                            <ul class="list-unstyled mb-4 pricing-feature-list">
-                                <li><span>10</span> Hosted Domains</li>
-                                <li><span>25 GB</span> RAID 10 Storage</li>
-                                <li><span>200 GB</span> Bandwidth</li>
-                                <li><span>1 Free</span> Domain included</li>
-                                <li><span>Super Fast</span> SSD Storage</li>
-                                <li><span>Free</span> SSL Certificate</li>
-                                <li>99.95% Uptime</li>
-                                <li>24/7 Phone Support</li>
-                            </ul>
-                            <a href="#" class="btn outline-btn mb-3" target="_blank">Purchase now</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-12">
+                <div class="col-lg-6 col-md-6 col-12 mb-3">
                     <div class="card popular-price text-center single-pricing-pack">
                         <div class="pt-5">
-                            <h5 class="mb-0">Cloud Business <span class="badge color-1 color-1-bg">Popular</span></h5>
-                            <p class="mb-0">High traffic corporate sites</p>
+                            <h5 class="mb-0">Gratuit <span class="badge text-white bg-success">Gratuit </span></h5>
                         </div>
                         <div class="card-header pb-4 border-0 pricing-header">
-                            <div class="price text-center mb-0 monthly-price">$49<span>/month</span></div>
-                            <div class="price text-center mb-0 yearly-price">$159<span>/yearly</span></div>
-                            <div class="price text-center mb-0 biannual-price">$299<span>/biannual</span></div>
-                            <div class="price text-center mb-0 triennial-price">$429<span>/triennial</span></div>
+                            <div class="price text-center mb-0 monthly-price">Gratuit pour<span>:durée</span></div>
                         </div>
                         <div class="card-body">
                             <ul class="list-unstyled mb-4 pricing-feature-list">
-                                <li><span>30</span> Hosted Domains</li>
-                                <li><span>75 GB</span> RAID 10 Storage</li>
-                                <li><span>700 GB</span> Bandwidth</li>
-                                <li><span>3 Free</span> Domain included</li>
-                                <li><span>Super Fast</span> SSD Storage</li>
-                                <li><span>Free</span> SSL Certificate</li>
-                                <li>99.95% Uptime</li>
-                                <li>24/7 Phone Support</li>
+                                <li><span class="text-success">✔ 1</span> Sites commerciaux</li>
+                                <li><span class="text-success">✔ 1</span> Utilisateurs</li>
+                                <li><span class="text-success">✔ 10</span> Des produits</li>
+                                <li><span class="text-success">✔ 10</span> Factures</li>
                             </ul>
-                            <a href="#" class="btn primary-solid-btn mb-3" target="_blank">Purchase now</a>
+                            <a href="https://demo.pylcom.app/business/register?package=1" class="btn primary-solid-btn mb-3" target="_blank">Acheter maintenant</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-12">
-                    <div class="card text-center single-pricing-pack">
+                <div class="col-lg-6 col-md-6 col-12 mb-3">
+                    <div class="card popular-price text-center single-pricing-pack">
                         <div class="pt-5">
-                            <h5 class="mb-0">Cloud Enterprise</h5>
-                            <p class="mb-0">Enterprise content management</p>
+                            <h5 class="mb-0">Basic <span class="badge color-1 color-1-bg">Payant</span></h5>
                         </div>
                         <div class="card-header pb-4 border-0 pricing-header">
-                            <div class="price text-center mb-0 monthly-price">$69<span>/month</span></div>
-                            <div class="price text-center mb-0 yearly-price">$259<span>/yearly</span></div>
-                            <div class="price text-center mb-0 biannual-price">$499<span>/biannual</span></div>
-                            <div class="price text-center mb-0 triennial-price">$739<span>/triennial</span></div>
+                            <div class="price text-center mb-0 monthly-price">DA 20 000.00<span>/Années</span></div>
                         </div>
                         <div class="card-body">
                             <ul class="list-unstyled mb-4 pricing-feature-list">
-                                <li><span>60</span> Hosted Domains</li>
-                                <li><span>175 GB</span> RAID 10 Storage</li>
-                                <li><span>1500 GB</span> Bandwidth</li>
-                                <li><span>6 Free</span> Domain included</li>
-                                <li><span>Super Fast</span> SSD Storage</li>
-                                <li><span>Free</span> SSL Certificate</li>
-                                <li>99.95% Uptime</li>
-                                <li>24/7 Phone Support</li>
+                                <li><span class="text-danger">✔ 1</span> Sites commerciaux</li>
+                                <li><span class="text-danger">✔ 2</span> Utilisateurs</li>
+                                <li><span class="text-danger">✔ </span> Illimité Des produits</li>
+                                <li><span class="text-danger">✔ </span> Illimité Factures</li>
                             </ul>
-                            <a href="#" class="btn outline-btn mb-3" target="_blank">Purchase now</a>
+                            <a href="https://demo.pylcom.app/business/register?package=2" class="btn primary-solid-btn mb-3" target="_blank">Acheter maintenant</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12 mb-3">
+                    <div class="card popular-price text-center single-pricing-pack">
+                        <div class="pt-5">
+                            <h5 class="mb-0">Standard <span class="badge color-1 color-1-bg">Payant</span></h5>
+
+                        </div>
+                        <div class="card-header pb-4 border-0 pricing-header">
+                            <div class="price text-center mb-0 monthly-price">DA 35 000.00 <span>/Années</span></div>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-unstyled mb-4 pricing-feature-list">
+                                <li><span class="text-danger">✔ 1</span> Sites commerciaux</li>
+                                <li><span class="text-danger">✔ 8</span> Utilisateurs</li>
+                                <li><span class="text-danger">✔ </span> Illimité Des produits</li>
+                                <li><span class="text-danger">✔ </span> Illimité Factures</li>
+                            </ul>
+                            <a href="https://demo.pylcom.app/business/register?package=3" class="btn primary-solid-btn mb-3" target="_blank">Acheter maintenant</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12 mb-3">
+                    <div class="card popular-price text-center single-pricing-pack">
+                        <div class="pt-5">
+                            <h5 class="mb-0">Professionnel <span class="badge color-1 color-1-bg">Payant</span></h5>
+                        </div>
+                        <div class="card-header pb-4 border-0 pricing-header">
+                            <div class="price text-center mb-0 monthly-price">DA 55 000.00<span>/Années</span></div>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-unstyled mb-4 pricing-feature-list">
+                                <li><span class="text-danger">✔ 1</span> Sites commerciaux</li>
+                                <li><span class="text-danger">✔ 8</span> Utilisateurs</li>
+                                <li><span class="text-danger">✔ </span> Illimité Des produits</li>
+                                <li><span class="text-danger">✔ </span> Illimité Factures</li>
+                                <li><span class="text-danger">✔ </span> Module Crm</li>
+                            </ul>
+                            <a href="https://demo.pylcom.app/business/register?package=4" class="btn primary-solid-btn mb-3" target="_blank">Acheter maintenant</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-12">
                     <div class="support-cta mt-5">
-                        <h5 class="mb-1 d-flex align-items-center justify-content-center"><span class="ti-loop color-primary mr-3 icon-sm"></span>Choose Your <a href="pricing-comparison.html" class="ml-2">Compare Hosting Plans</a></h5>
+                        <h5 class="mb-1 d-flex align-items-center justify-content-center"><span class="ti-loop color-primary mr-3 icon-sm"></span>Choisissez votre  <a href="#" class="ml-2">plans de pylcom</a></h5>
                     </div>
                 </div>
             </div>
